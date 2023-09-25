37ba9fa6717da439d1ad1fb5d1e3b5faf81d5556
jeycys <julioscvc75@gmail.com>
2023-09-25 07:30:49 -0600

    P003_Mayor_que_menor_que
    si

---
+++ 000_Hola_mundo_V0/P003_Mayor que Menor que/P003_Mayor que Menor que.cpp
@@ -0,0 +1,47 @@
+// P003_Mayor que Menor que.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+   
+	float n1;
+	float n2;
+
+	std::cout << "Hola usuario, hoy vamos a comparar dos numeros para ver cual es mayor y cual es menor.\n";
+	std::cout << "Dame dos numeros: \n";
+	std::cin >> n1;
+	std::cout << "\n";
+	std::cin >> n2;
+	std::cout << "\n";
+	if (n1 > n2)
+	{
+		std::cout << n1 << " es mayor que " << n2;
+
+	}
+	else
+	{
+		std::cout << n2 << " es mayor que " << n1;
+	}
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
