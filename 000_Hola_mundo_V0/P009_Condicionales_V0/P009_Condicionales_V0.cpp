f49e3bb333539e628c94a42f37a23204add19066
jeycys <143419942+jeycys@users.noreply.github.com>
2023-10-02 10:05:24 -0600

    P009_Condicionales_V1.cpp

--- 000_Hola_mundo_V0/P009_Condicionales_V0/P009_Condicionales_V0.cpp
+++ 000_Hola_mundo_V0/P009_Condicionales_V0/P009_Condicionales_V0.cpp
@@ -1,5 +1,6 @@
 // P009_Condicionales_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// by julio
+// se hizo una pequeña calculadora
 
 #include <iostream>
 #include <math.h>
@@ -20,32 +21,39 @@ int main()
     float mayorq = 0;
 
     std::cout << "Ingrese dos numeros para sacar suma, resta, multiplicacion, division, exponente (el segundo numero sera la potencia), la raiz cuadrada, el valor absoluto y mayor que menor que: \n";
-    std::cin >> n1;
-    std::cout << "\n";
-    std::cin >> n2;
-    std::cout << "\n";
-    std::cout << "\n";
-    suma = n1 + n2;
-    resta = n1 - n2;
-    multi = n1 * n2;
-    divi = n1 / n2;
-    expo = std::pow(n1, n2);
-    raiz = sqrt(suma);
-    abss = abs(suma);
+    bool seguir = true;
+    while (seguir)
+    {
+        std::cin >> n1;
+        std::cout << "\n";
+        std::cin >> n2;
+        std::cout << "\n";
+        std::cout << "\n";
+        suma = n1 + n2;
+        resta = n1 - n2;
+        multi = n1 * n2;
+        divi = n1 / n2;
+        expo = std::pow(n1, n2);
+        raiz = sqrt(suma);
+        abss = abs(suma);
 
-    std::cout << "El resultado de la suma es: " << suma << std::endl;
-    std::cout << "El resultado de la resta es: " << resta << std::endl;
-    std::cout << "El resultado de la multiplicacion es: " << multi << std::endl;
-    std::cout << "El resultado de la division es: " << divi << std::endl;
-    std::cout << "El resultado del exponente de " << n1 << " que es " << n2 << " es " << expo << std::endl;
-    std::cout << "El resultado de la raiz de " << suma << " es " << raiz << std::endl;
-    std::cout << "El valor absoluto de la suma es " << abss << std::endl;
-    if (n1 < n2)
-    {
-        std::cout << n1 << " es menor que " << n2;
-    }
-    else
-    {
-        std::cout << n2 << " es menor que " << n1;
+        std::cout << "El resultado de la suma es: " << suma << std::endl;
+        std::cout << "El resultado de la resta es: " << resta << std::endl;
+        std::cout << "El resultado de la multiplicacion es: " << multi << std::endl;
+        std::cout << "El resultado de la division es: " << divi << std::endl;
+        std::cout << "El resultado del exponente de " << n1 << " que es " << n2 << " es " << expo << std::endl;
+        std::cout << "El resultado de la raiz de " << suma << " es " << raiz << std::endl;
+        std::cout << "El valor absoluto de la suma es " << abss << std::endl;
+        if (n1 < n2)
+        {
+            std::cout << n1 << " es menor que " << n2;
+        }
+        else
+        {
+            std::cout << n2 << " es menor que " << n1;
+        }
+    std::cout << "\n¿Quieres hacerlo otra vez con otros numeros?\n 1-si\n 0-no\n";
+    std::cin >> seguir;
+    std::cout << "Perfecto, ingresa nuevamente 2 numeros\n";
     }
 }
