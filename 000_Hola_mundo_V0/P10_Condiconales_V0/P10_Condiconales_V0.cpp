a6c0b741b6b5b02f2ad1f4ce68b30edd1eaaabbb
jeycys <143419942+jeycys@users.noreply.github.com>
2023-10-02 08:52:16 -0600

    P10_Condiconales_V1.cpp

--- 000_Hola_mundo_V0/P10_Condiconales_V0/P10_Condiconales_V0.cpp
+++ 000_Hola_mundo_V0/P10_Condiconales_V0/P10_Condiconales_V0.cpp
@@ -1,5 +1,6 @@
-// P10_Condiconales_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// by julio
+// se hizo un login
 
 #include <iostream>
 #include <string>
@@ -13,38 +14,49 @@ int main()
     string contra;
     string usu2;
     string contra2;
-    std::cout << "Hola usuario, para crear una nueva cuenta ingrese un usuario y su nueva contraseña\n";
-    std::cout << "Ingrese su nuevo usuario:  ";
-    std::cout << "\n";
-    std::cin >> usu;
-    std::cout << "Ahora ingrese su contraseña:  " << contra;
-    std::cout << "\n";
-    std::cin >> contra;
-    std::cout << "Confirme su usario  su contraseña:\n";
-    std::cin >> usu2;
-    std::cout << "\n";
-    std::cin >> contra2;
-    if (usu == usu2)
-    {
-        std::cout << "Tu usuario es correcto";
 
-    }
-    else
-    {
-        std::cout << "Tu usario es incorrecto, intentelo más tarde \n";
-    }
 
-    if (contra == contra2)
+    bool seguir = true;
+    while (seguir)
     {
-        std::cout << ", tu contraseña es correcta";
+        std::cout << "Hola usuario, para crear una nueva cuenta ingrese un usuario y su nueva contraseña\n";
+        std::cout << "Ingrese su nuevo usuario:  ";
+        std::cout << "\n";
+        std::cin >> usu;
+        std::cout << "Ahora ingrese su contraseña:  " << contra;
+        std::cout << "\n";
+        std::cin >> contra;
+        std::cout << "Confirme su usario  su contraseña:\n";
+        std::cin >> usu2;
+        std::cout << "\n";
+        std::cin >> contra2;
 
-    }
-    else
-    {
-        std::cout << "La contraseña esta incorrecta, intentelo más tarde";
-    }
 
-    
+        if (usu == usu2)
+        {
+            std::cout << "Tu usuario es correcto";
+
+        }
+        else
+        {
+            std::cout << "Tu usario es incorrecto, intentelo más tarde \n";
+        }
+
+
+        if (contra == contra2)
+        {
+            std::cout << ", tu contraseña es correcta";
+
+        }
+        else
+        {
+            std::cout << " La contraseña esta incorrecta, intentelo más tarde";
+
+        }
+
+        std::cout << " Deseas cambiar alguna de las dos otra vez??\n 1- si\n 0- NO\n";
+        std::cin >> seguir;
+    }
 
 
 
