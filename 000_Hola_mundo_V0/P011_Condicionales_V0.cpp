0d01ef97664e09dfbc3de76d7175bf3e62d47f06
jeycys <143419942+jeycys@users.noreply.github.com>
2023-10-04 07:51:23 -0600

    P011_Condicionales_V0.cpp

--- 000_Hola_mundo_V0/P011_Condicionales_V0.cpp
+++ 000_Hola_mundo_V0/P011_Condicionales_V0.cpp
@@ -1,7 +1,9 @@
 // P011_Condicionales_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+//  by julio
+// se hizo un akinator
 
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -9,66 +11,66 @@ int main()
     int b = 0;
     int c = 0;
     int d = 0;
+    std::string sino;
     bool adivina = false;
     std::cout << "Hola, soy tu amigo Akinator\n Intentare adivinar el color en el que estes pensando\n";
     std::cout << "Estas listo??\n 0 - No \n 1 - Si\n";
     std::cin >> adivina;
     std::cout << "las respuestas seran 0 para no y 1 para si\n";
-
-    std::cout << "¿Tu color esta relacionado con un arbol?";
-    std::cin >> a;
-    if (a == 1)
+    while (adivina)
     {
-        std::cout << "¿Tu color representa a una hoja?";
+        std::cout << "¿Tu color esta relacionado con un arbol?";
         std::cin >> a;
         if (a == 1)
         {
-            std::cout << "Tu color es el verde";
+            std::cout << "¿Tu color representa a una hoja?";
+            std::cin >> a;
+            if (a == 1)
+            {
+                std::cout << "Tu color es el verde";
+            }
+            else
+            {
+                std::cout << "Tu color es el marron";
+            }
         }
         else
         {
-            std::cout << "Tu color es el marron";
-        }
-    }
-    else
-    {
-        std::cout << "¿Tu color tiene que ver con el agua?";
-        std::cin >> b;
-        if (b == 1)
-        {
-            std::cout << "Tu color es el azul";
-        }
-        else {
-
-            std::cout << "'Tu coloor tiene que ver con el jing y el yang?";
-            std::cin >> c;
-            if (c == 1)
+            std::cout << "¿Tu color tiene que ver con el agua?";
+            std::cin >> b;
+            if (b == 1)
             {
-                std::cout << "¿Tu color representa la maldad?";
+                std::cout << "Tu color es el azul";
+            }
+            else
+            {
+
+                std::cout << "'Tu coloor tiene que ver con el jing y el yang?";
                 std::cin >> c;
                 if (c == 1)
                 {
-                    std::cout << "Tu color es el negro";
+                    std::cout << "¿Tu color representa la maldad?";
+                    std::cin >> c;
+                    if (c == 1)
+                    {
+                        std::cout << "Tu color es el negro";
+                    }
+                    else
+                    {
+                        std::cout << "Tu color es el blanco";
+                    }
+
+
                 }
-                else
-                {
-                    std::cout << "Tu color es el blanco";
-                }
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
+            
         }
+        std::cout << "\n¿Adivine?";
+        std::cin >> sino;
+        std::cout << "¿Quieres jugar otra vez?";
+        std::cin >> adivina;
+    }
+  }
 
 
 
